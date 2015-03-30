3cc0496eea929a4e7b5f001e92123d9dac824ebc
D'Haese Willem <willem.dhaese@gmail.com>
2015-03-30 19:38:40 +0200

    Cleanup and compatibility with CentOS 7

--- count_yum_updates.sh
+++ count_yum_updates.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Script name: 		count_yum_updates.sh
-# Version: 			1.15.01.09
+# Version: 			0.15.03.30
 # Created on: 		15/11/2014
 # Author: 			Willem D'Haese
-# Purpose:  		Bash script that will output total number of yum updates.
-# On Github:		https://github.com/willemdh/generate_motd
+# Purpose: 			Bash script that will dynamically generate a message of they day for users logging in.
+# On GitHub: 		https://github.com/willemdh/generate_motd
 # On OutsideIT:		http://outsideit.net/generate-motd
-# History:
-#	15/11/2014 => Script creation
-#	18/11/2014 => if statement in case 0 updates available (else -1)
-#	09/01/2015 => Cleanup script header
+# Recent History:
+#       15/11/2014 => Script creation
+#       18/11/2014 => if statement in case 0 updates available (else -1)
+#		30/03/2015 => Cleanup, better compatibility with CentOS 7
 # Copyright:
 #	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
 #	by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed 
@@ -18,15 +18,11 @@
 #	PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public 
 #	License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-IFACE=eth0
-
-if [ -n "$(/sbin/ifconfig $IFACE | /bin/grep RUNNING)" ]; then
-        YUMCOUNT=`/usr/bin/yum -d 0 check-update 2>/dev/null | echo $(($(wc -l)-1))`
-        if [ $YUMCOUNT == -1 ]; then
-                YUMCOUNT=0
-        fi
-        echo "$YUMCOUNT"
+YUMCOUNT=`/usr/bin/yum -d 0 check-update 2>/dev/null | echo $(($(wc -l)-1))`
+if [ $YUMCOUNT == -1 ]; then
+        YUMCOUNT=0
 fi
-
+echo "$YUMCOUNT"
 exit 0
+
 
