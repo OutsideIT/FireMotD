45cce37a0f7def8da418c9d91bbc3647b0b424aa
willemdh <willem.dhaese@gmail.com>
2015-07-21 14:55:02 +0200

    v2.0.7.21 => Introduction of Blue and Red theme

--- generate_motd.sh
+++ generate_motd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script name:          generate_motd.sh
-# Version:              v1.7.13
+# Version:              v2.0.7.21
 # Created on:           10/02/2014
 # Author:               Willem D'Haese
 # Purpose:              Bash script that will dynamically generate a message of they day for users logging in.
@@ -13,23 +13,24 @@
 #       30/03/2015 => Replaced ifconfig with ip route so it works on CentOS 6 and 7
 #       15/04/2015 => Prep for GitHub release and 16 color version
 #	13/07/2015 => Massive cleanup, prep for custom color themes and added days to uptime and number of cores
+#	21/07/2015 => Introduction of 'Red' and 'Blue' themes
 # Copyright:
 #       This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
 #       by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed 
 #       in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
 #       PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public 
 #       License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Examples:
+#	Blanco 	=> ./generate_motd.sh
+#	Blue 	=> ./generate_motd.sh Blue
+#       Red    	=> ./generate_motd.sh Red
 
 # Some fun from the past
 # for i in {17..21} {21..17} ; do ShortBlueScheme+="\e[38;5;${i}m#\e[0m"  ; done ;
 # for i in {17..21} {21..17} ; do BlueScheme+="\e[38;5;${i}m#\e[0m\e[38;5;${i}m#\e[0m"  ; done ;
 # for i in {17..21} {21..17} ; do LongBlueScheme+="\e[38;5;${i}m#\e[0m\e[38;5;${i}m#\e[0m\e[38;5;${i}m#"  ; done ;
 
-# 16 color supported blue color codes
-#\e[0;34m => Blue
-#\e[0;36m => Cyan
-#\e[1;34m => Light Blue
-#\e[1;36m => Light Cyan
+Theme=$1
 
 # CPU Utilisation
 CpuUtil=`LANG=en_GB.UTF-8 mpstat 1 1 | awk '$2 ~ /CPU/ { for(i=1;i<=NF;i++) { if ($i ~ /%idle/) field=i } } $2 ~ /all/ { print 100 - $field}' | tail -1`
@@ -65,74 +66,77 @@ Hostname=`hostname`
 HostChars=$((${#Hostname} + 8))
 LeftoverChars=$((MaxLeftOverChars - HostCHars -10))
 
-# 16 Color Blue Frame Scheme
-# Blue
-Sch1="\e[0;34m#####"
-# Light Blue
-Sch2="\e[1;34m#####"
-# Light Cyan
-Sch3="\e[1;36m#####"
-# Cyan
-Sch4="\e[0;36m#####"
-# Pre-Host Scheme
-PrHS=$Sch1$Sch1$Sch2$Sch2
-# Host Scheme Top
-HST="\e[1;36m`head -c $HostChars /dev/zero|tr '\0' '#'`"
-# Host Scheme Top Filler
-HSF="\e[1;36m###"
-# Host Scheme Bot
-HSB="\e[1;34m`head -c $HostChars /dev/zero|tr '\0' '#'`"
-# Post Host Scheme
-PHS="\e[1;34m`head -c $LeftoverChars /dev/zero|tr '\0' '#'`"
-# Front Scheme
-FrS="\e[0;34m##"
-# Equal Scheme
-ES="\e[1;34m="
+if [[ $Theme = "Blue" ]] ; then 
 
-# 16 Color Red Frame Scheme
-# Red
-Sch1="\e[0;31m#####"
-# Light Red
-Sch2="\e[1;31m#####"
-# Light Yellow
-Sch3="\e[1;33m#####"
-# Yellow
-Sch4="\e[0;33m#####"
-# Pre-Host Scheme
-PrHS=$Sch1$Sch1$Sch2$Sch2
-# Host Scheme Top
-HST="\e[1;33m`head -c $HostChars /dev/zero|tr '\0' '#'`"
-# Host Scheme Top Filler
-HSF="\e[1;33m###"
-# Host Scheme Bot
-HSB="\e[0;31m`head -c $HostChars /dev/zero|tr '\0' '#'`"
-# Post Host Scheme
-PHS="\e[2;31m`head -c $LeftoverChars /dev/zero|tr '\0' '#'`"
-# Front Scheme
-FrS="\e[0;31m##"
-# Equal Scheme
-ES="\e[1;31m="
+	# 16 Color Blue Frame Scheme
+	# Blue
+	Sch1="\e[0;34m#####"
+	# Light Blue
+	Sch2="\e[1;34m#####"
+	# Light Cyan
+	Sch3="\e[1;36m#####"
+	# Cyan
+	Sch4="\e[0;36m#####"
+	# Pre-Host Scheme
+	PrHS=$Sch1$Sch1$Sch2$Sch2
+	# Host Scheme Top
+	HST="\e[1;36m`head -c $HostChars /dev/zero|tr '\0' '#'`"
+	# Host Scheme Top Filler
+	HSF="\e[1;36m###"
+	# Host Scheme Bot
+	HSB="\e[1;34m`head -c $HostChars /dev/zero|tr '\0' '#'`"
+	# Post Host Scheme
+	PHS="\e[1;34m`head -c $LeftoverChars /dev/zero|tr '\0' '#'`"
+	# Front Scheme
+	FrS="\e[0;34m##"
+	# Equal Scheme
+	ES="\e[1;34m="
 
+	# 16 Color Green Value Scheme      
+	# Host Color                       
+	HC="\e[1;32m"                      
+	# Green Value Color                
+	VC="\e[0;32m"                      
+	# Light Green Value Color          
+	VCL="\e[1;32m"                     
+	# Light Yellow Key Color           
+	KS="\e[1;33m"                      
 
-# 16 Color Green Value Scheme
-# Host Color
-HC="\e[1;32m"
-# Green Value Color
-VC="\e[0;32m"
-# Light Green Value Color
-VCL="\e[1;32m"
-# Light Yellow Key Color
-KS="\e[1;33m"
-
-# 16 Color Green Value Scheme
-# Host Color
-HC="\e[1;37m"
-# Green Value Color
-VC="\e[0;33m"
-# Light Green Value Color
-VCL="\e[1;33m"
-# Light Yellow Key Color
-KS="\e[0;37m"
+elif [[ $Theme = "Red" ]] ; then 
+	# 16 Color Red Frame Scheme
+	# Red
+	Sch1="\e[0;31m#####"
+	# Light Red
+	Sch2="\e[1;31m#####"
+	# Light Yellow
+	Sch3="\e[1;33m#####"
+	# Yellow
+	Sch4="\e[0;33m#####"
+	# Pre-Host Scheme
+	PrHS=$Sch1$Sch1$Sch2$Sch2
+	# Host Scheme Top
+	HST="\e[1;33m`head -c $HostChars /dev/zero|tr '\0' '#'`"
+	# Host Scheme Top Filler
+	HSF="\e[1;33m###"
+	# Host Scheme Bot
+	HSB="\e[0;31m`head -c $HostChars /dev/zero|tr '\0' '#'`"
+	# Post Host Scheme
+	PHS="\e[2;31m`head -c $LeftoverChars /dev/zero|tr '\0' '#'`"
+	# Front Scheme
+	FrS="\e[0;31m##"
+	# Equal Scheme
+	ES="\e[1;31m="
+	
+	# 16 Color Yellow Value Scheme
+	# Host Color
+	HC="\e[1;37m"
+	# Yellow Value Color
+	VC="\e[0;33m"
+	# Light Yellow Value Color
+	VCL="\e[1;33m"
+	# Light Yellow Key Color
+	KS="\e[0;37m"
+fi
 
 # Output
 echo -e "
@@ -152,8 +156,7 @@ $FrS     ${KS}Updates $ES ${VCL}`cat /tmp/yum_updates.txt` ${VC}yum updates avai
 $FrS    ${KS}Sessions $ES ${VCL}`who | grep $USER | wc -l` ${VC}sessions
 $FrS   ${KS}Processes $ES ${VCL}`ps -Afl | wc -l` ${VC}running processes of ${VCL}`ulimit -u` ${VC}maximum processes
 $PrHS$Sch2$HSB$Sch2$PHS$Sch1
-\e[0;37m
-"
+\e[0;37m"
 
 
 
