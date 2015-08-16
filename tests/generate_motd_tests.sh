97e32f1d7e26166feae02e041092309f18c01f23
D'Haese Willem <willem.dhaese@gmail.com>
2015-08-16 12:08:44 +0200

    Travis test 3

--- tests/generate_motd_tests.sh
+++ tests/generate_motd_tests.sh
@@ -7,12 +7,12 @@ testSampleScriptParameters() {
 
   echo "Executing 3 Asserts..."
 
-  # Add asserts here ...
+  assertTrue 'Check default DEMO_PATH' "[ '${DEMO_PATH}' == '/bin/bash' ]"
+  assertTrue 'Check valid DEMO_INT values' "[ $DEMO_INT -ge 0 -a $DEMO_INT -le 2 ]"
+  assertTrue 'Check that DEMO_ARRAY is not empty' "[ ${#DEMO_ARRAY[@]} -ne 0 ]"
 }
 
-function testWeCanWriteTests () {
-    assertEquals "it works" "it works"
-}
+
 
 # Execute shunit2 to run the tests
 . shunit2-2.1.6/src/shunit2
