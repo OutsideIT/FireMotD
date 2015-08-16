762c08b5636dfb65bd815b505bb0a98a0716cd3d
willemdh <willem.dhaese@gmail.com>
2015-08-16 21:05:52 +0200

    Merged yum count

--- generate_motd.sh
+++ generate_motd.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Script name:          generate_motd.sh
-# Version:              v2.17.150813
+# Version:              v2.21.150816
 # Created on:           10/02/2014
 # Author:               Willem D'Haese
 # Purpose:              Bash script that will dynamically generate a message of they day for users logging in.
 # On GitHub:            https://github.com/willemdh/generate_motd
 # On OutsideIT:         http://outsideit.net/generate-motd
 # Recent History:
-#       13/07/2015 => Massive cleanup, prep for custom color themes and added days to uptime and number of cores
-#       21/07/2015 => Introduction of 'Red' and 'Blue' themes
-#       30/07/2015 => Added exticode and cleanup
-#	12/08/2015 => Added version to output
-# 	13/08/2015 => Finalized version insertion
+#       21/07/15 => Introduction of 'Red' and 'Blue' themes
+#       30/07/15 => Added exticode and cleanup
+#	12/08/15 => Added version to output
+# 	13/08/15 => Finalized version insertion
+#	16/08/15 => Merged yum count into this script
 # Copyright:
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any 
@@ -24,6 +24,7 @@
 #       Blanco  => ./generate_motd.sh
 #       Blue    => ./generate_motd.sh Blue
 #       Red     => ./generate_motd.sh Red
+#	yum	=> ./generate_motd.sh yum
 
 # Some fun from the past
 # for i in {17..21} {21..17} ; do ShortBlueScheme+="\e[38;5;${i}m#\e[0m"  ; done ;
@@ -31,6 +32,15 @@
 # for i in {17..21} {21..17} ; do LongBlueScheme+="\e[38;5;${i}m#\e[0m\e[38;5;${i}m#\e[0m\e[38;5;${i}m#"  ; done ;
 
 Theme=$1
+
+if [[ $Theme = "yum" ]] ; then
+	YumCount=`/usr/bin/yum -d 0 check-update 2>/dev/null | echo $(($(wc -l)-1))`
+	if [ $YumCount == -1 ]; then
+        	YumCount=0
+	fi
+	echo "$YumCount"
+	exit 0
+fi
 
 ScriptName="`readlink -e $0`"
 ScriptVersion=" `cat $ScriptName | grep "# Version:" | awk {'print $3'} | tr -cd '[[:digit:].-]' | sed 's/.\{2\}$//'` "
