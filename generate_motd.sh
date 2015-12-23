99ffeb2085cdf2ddb2229c2ff4f1c274782acdd1
willemdh <willem.dhaese@gmail.com>
2015-12-23 17:38:49 +0100

    v3.01.151223 => Re-introduction of original theme

--- generate_motd.sh
+++ generate_motd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script name:          generate_motd.sh
-# Version:              v2.29.151222
+# Version:              v3.01.151223
 # Created on:           10/02/2014
 # Author:               Willem D'Haese
 # Purpose:              Bash script that will dynamically generate a message
@@ -8,11 +8,11 @@
 # On GitHub:            https://github.com/willemdh/generate_motd
 # On OutsideIT:         https://outsideit.net/generate-motd
 # Recent History:
-#   07/11/15 => Width two chars smaller, added dmesg platform info and writelog
 #   16/11/15 => Support for Fujitsu servers
 #   01/12/15 => Separated OsVersion and replaced cut with sed for DMI mesg
 #   21/12/15 => Added PHP version
 #   22/12/15 => Cleanup  for release
+#   23/12/15 => Re-introduction of original theme
 # Copyright:
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
@@ -24,14 +24,15 @@
 # GNU General Public License along with this program.  If not, see
 # <http://www.gnu.org/licenses/>.
 # Examples:
-#   Blanco  => ./generate_motd.sh
-#   Blue    => ./generate_motd.sh Blue
-#   Red     => ./generate_motd.sh Red
-#   yum     => ./generate_motd.sh yum
+#   Blanco  	=> ./generate_motd.sh
+#   Blue    	=> ./generate_motd.sh Blue
+#   Red     	=> ./generate_motd.sh Red
+#   Original	=> ./generate_motd.sh Original
+#   yum     	=> ./generate_motd.sh yum
 
 Theme=$1
 Verbose=0
-if [[ $Theme = "yum" ]] ; then
+if [[ "$Theme" = "yum" || "$Theme" = "Yum" || "$Theme" = "YUM" ]] ; then
         YumCount=`/usr/bin/yum -d 0 check-update 2>/dev/null | echo $(($(wc -l)-1))`
         if [ $YumCount == -1 ]; then
                 YumCount=0
@@ -59,6 +60,9 @@ writelog () {
 ScriptName="`readlink -e $0`"
 ScriptVersion=" `cat $ScriptName | grep "# Version:" | awk {'print $3'} | tr -cd '[[:digit:].-]' | sed 's/.\{2\}$//'` "
 OsVersion=`cat /etc/*release | head -n 1`
+IpAddress=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
+Kernel=$(uname -rs)
+Uptime=$(awk '{print int($1/86400)" day(s) "int($1%86400/3600)":"int(($1%3600)/60)":"int($1%60)}' /proc/uptime)
 Dmi=`dmesg | grep "DMI:"`
 if [[ $Dmi == *"QEMU"* ]] ; then
   Platform=`dmesg | grep "DMI:" | sed 's/^.*QEMU/QEMU/' | sed 's/, B.*//'`
@@ -71,6 +75,7 @@ else
 fi
 CpuUtil=`LANG=en_GB.UTF-8 mpstat 1 1 | awk '$2 ~ /CPU/ { for(i=1;i<=NF;i++) { if ($i ~ /%idle/) field=i } } $2 ~ /all/ { print 100 - $field}' | tail -1`
 CpuProc="`cat /proc/cpuinfo | grep processor | wc -l` core(s)."
+CpuLoad=$(uptime | grep -ohe '[s:][: ].*' | awk '{ print "1m: "$2 " 5m: "$3 " 15m: " $4}')
 MemFreeB=`cat /proc/meminfo | grep MemFree | awk {'print $2'}`
 MemTotalB=`cat /proc/meminfo | grep MemTotal | awk {'print $2'}`
 MemUsedB=`expr $MemTotalB - $MemFreeB`
@@ -170,19 +175,47 @@ elif [[ $Theme = "Red" ]] ; then
         KS="\e[0;37m"
 	# Version Color
 	SVC="\e[1;33m"
+elif [[ $Theme = "Original" ]] ; then
+	for i in {18..21} {21..18} ; do ShortBlueScheme+="\e[38;5;${i}m#\e[0m"  ; done ;
+	for i in {17..21} {21..17} ; do BlueScheme+="\e[38;5;${i}m#\e[0m\e[38;5;${i}m#\e[0m"  ; done ;
+	for i in {17..21} {21..17} ; do LongBlueScheme+="\e[38;5;${i}m#\e[0m\e[38;5;${i}m#\e[0m\e[38;5;${i}m#"  ; done ;
+
+	echo -e "
+$BlueScheme$LongBlueScheme$BlueScheme$ShortBlueScheme
+$BlueScheme \e[38;5;93m $Hostname $BlueScheme \e[38;5;98m$ScriptVersion
+$BlueScheme$LongBlueScheme$BlueScheme$ShortBlueScheme
+\e[0;38;5;17m##          \e[38;5;39mIp \e[38;5;93m= \e[38;5;33m$IpAddress
+\e[0;38;5;17m##     \e[38;5;39mRelease \e[38;5;93m= \e[38;5;27m$OsVersion
+\e[0;38;5;17m##      \e[38;5;39mKernel \e[38;5;93m= \e[38;5;27m$Kernel
+\e[0;38;5;17m##    \e[38;5;39mPlatform \e[38;5;93m= \e[38;5;27m$Platform
+\e[0;38;5;17m##      \e[38;5;39mUptime \e[38;5;93m= \e[38;5;27m$Uptime
+\e[0;38;5;17m##    \e[38;5;39mCPU Util \e[38;5;93m= \e[38;5;33m${CpuUtil}\e[38;5;27m% average CPU usage over $CpuProc
+\e[0;38;5;17m##    \e[38;5;39mCPU Load \e[38;5;93m= \e[38;5;27m$CpuLoad
+\e[0;38;5;17m##      \e[38;5;39mMemory \e[38;5;93m= \e[38;5;27mFree: \e[38;5;33m${MemFree}\e[38;5;27mGB, Used: \e[38;5;33m${MemUsed}\e[38;5;27mGB, Total: \e[38;5;33m${MemTotal}\e[38;5;27mGB
+\e[0;38;5;17m##        \e[38;5;39mSwap \e[38;5;93m= \e[38;5;27mFree: \e[38;5;33m${SwapFree}\e[38;5;27mGB, Used: \e[38;5;33m${SwapUsed}\e[38;5;27mGB, Total: \e[38;5;33m${SwapTotal}\e[38;5;27mGB
+\e[0;38;5;17m##        \e[38;5;39mRoot \e[38;5;93m= \e[38;5;27mFree: \e[38;5;33m${RootFree}\e[38;5;27mGB, Used: \e[38;5;33m${RootUsed}\e[38;5;27mGB, Total: \e[38;5;33m${RootTotal}\e[38;5;27mGB
+\e[0;38;5;17m##     \e[38;5;39mUpdates \e[38;5;93m= \e[38;5;33m`cat /tmp/yum_updates.txt` \e[38;5;27myum updates available
+\e[0;38;5;17m##    \e[38;5;39mSessions \e[38;5;93m= \e[38;5;33m`who | grep $USER | wc -l`\e[38;5;27m sessions
+\e[0;38;5;17m##   \e[38;5;39mProcesses \e[38;5;93m= \e[38;5;33m`ps -Afl | wc -l`\e[38;5;27m running processes of \e[38;5;33m`ulimit -u`\e[38;5;27m maximum processes"
+if [[ $PhpVersion =~ ^[0-9.]+$ ]] ; then
+        echo -e "\e[0;38;5;17m##    \e[38;5;39mPHP Info \e[38;5;93m= \e[38;5;27mVersion: $PhpVersion"
+fi
+echo -e "$BlueScheme$LongBlueScheme$BlueScheme$ShortBlueScheme
+\e[0;37m"
+	exit 0
 fi
 
 echo -e "
 $PrHS$Sch2$HST$Sch2$PHS$Sch1
 $PrHS$Sch3$HSF $HC$Hostname $HSF$Sch3$HSF$HVF$SVC$ScriptVersion$Sch1
 $PrHS$Sch2$HST$Sch2$PHS$Sch1
-$FrS          ${KS}Ip $ES ${VCL}`ip route get 8.8.8.8 | head -1 | cut -d' ' -f8`
+$FrS          ${KS}Ip $ES ${VCL}$IpAddress
 $FrS     ${KS}Release $ES ${VC}$OsVersion
-$FrS      ${KS}Kernel $ES ${VC}`uname -rs`
+$FrS      ${KS}Kernel $ES ${VC}$Kernel
 $FrS    ${KS}Platform $ES ${VC}$Platform
-$FrS      ${KS}Uptime $ES ${VC}`awk '{print int($1/86400)" day(s) "int($1%86400/3600)":"int(($1%3600)/60)":"int($1%60)}' /proc/uptime`
+$FrS      ${KS}Uptime $ES ${VC}$Uptime
 $FrS    ${KS}CPU Util $ES ${VCL}$CpuUtil ${VC}% average CPU usage over $CpuProc
-$FrS    ${KS}CPU Load $ES ${VC}`uptime | grep -ohe '[s:][: ].*' | awk '{ print "1m: "$2 " 5m: "$3 " 15m: " $4}'`
+$FrS    ${KS}CPU Load $ES ${VC}$CpuLoad
 $FrS      ${KS}Memory $ES ${VC}Free: ${VCL}${MemFree}${VC} GB, Used: ${VCL}${MemUsed}${VC} GB, Total: ${VCL}${MemTotal}${VC} GB
 $FrS        ${KS}Swap $ES ${VC}Free: ${VCL}${SwapFree}${VC} GB, Used: ${VCL}${SwapUsed}${VC} GB, Total: ${VCL}${SwapTotal}${VC} GB
 $FrS        ${KS}Root $ES ${VC}Free: ${VCL}${RootFree}${VC} GB, Used: ${VCL}${RootUsed}${VC} GB, Total: ${VCL}${RootTotal}${VC} GB
@@ -192,6 +225,7 @@ $FrS   ${KS}Processes $ES ${VCL}`ps -Afl | wc -l` ${VC}running processes of ${VC
 if [[ $PhpVersion =~ ^[0-9.]+$ ]] ; then
 	echo -e "$FrS    ${KS}PHP Info $ES ${VC}Version: $PhpVersion"
 fi
-echo -e "$PrHS$Sch2$HSB$Sch2$PHS$Sch1"
+echo -e "$PrHS$Sch2$HSB$Sch2$PHS$Sch1
+\e[0;37m"
 
 exit 0
